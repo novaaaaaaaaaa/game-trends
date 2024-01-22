b687a7bb76f931f59097e938ebbabb989437bd5f
TheBatman12135 <150658593+TheBatman12135@users.noreply.github.com>
2024-01-22 10:58:01 +0000

    attempt to link images

--- game_trends/main/urls.py
+++ game_trends/main/urls.py
@@ -5,6 +5,7 @@ app_name = 'main'
 
 urlpatterns = [
     path('', views.dashboard, name='dashboard'),
+    path('dashboard', views.dashboard, name='dashboard'),
     path('profile', views.profile, name='profile'),
     path('friends', views.friends, name='friends'),
     path('games', views.games, name='games'),
